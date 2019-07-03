220bcfa59ce1d353c87501bd3fc899acc3a3010e
Kevin Sohn <kevin@centerid.com>
2019-07-03 15:07:41 -0700

    added more comments

--- 01-cli-api-call/index.js
+++ 01-cli-api-call/index.js
@@ -1,11 +1,31 @@
 #!/usr/bin/env node
+// Added shebang
 
 var request = require("request");
+// importing "request" module
 
 const URL = process.argv[2];
+// const of URL. Since node runs as "node filename -arg", [2] is the correct index
 
 console.log(URL)
+// logging for debugging purpose, to see if const actually worked
 
 request(URL, function(error, response, body) {
+  console.error('error:', error);
+  console.log('statusCode:', response && response.statusCode);
   console.log(body);
 });
+
+/*
+Used "request" module. The first parameter passing was a hard-coded URL,
+so I just replaced it with my own const, URL. I honestly just copied and pasted
+(error, response, body) from the library github, as referenced.
+
+First line is error. I would have to trigger it once the code enters error state,
+but I stil haven't figure out how.
+
+Second line of to return the status code of http call.
+
+Final line is to output the body of responses.
+
+*/
